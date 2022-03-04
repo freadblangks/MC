eb2ebfd74a71bd8f75a3edd9aff6dbe49d196575
devovh <devovh@gmail.com>
2022-03-04 09:41:09 +0100

    Core/Patch - Add Congrats On Level.

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -19,6 +19,7 @@
 void AddSC_Transmogrification();
 void AddSC_CrossfactionBattlegrounds();
 void AddSC_AutoBalance();
+void AddSC_AddCongratsOnLevelScripts();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
@@ -27,4 +28,5 @@ void AddCustomScripts()
     AddSC_Transmogrification();
 	AddSC_CrossfactionBattlegrounds();
     AddSC_AutoBalance();
+	AddSC_AddCongratsOnLevelScripts();
 }
